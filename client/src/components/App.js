1a8a18fde93e8f47d9b0f302554f89accfe3f169
Susan <susanwenzhang@gmail.com>
2020-03-05 18:19:51 -0500

    The rest of the files from front end that were in stash

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,29 +1,45 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import useApplicationData from '../hooks/useApplicationData';
 
 function App() {
+  const { links } = useApplicationData();
 
-    axios.request({
-        url: 'http://localhost:3001/users',
-        method: 'get',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Access-Control-Allow-Credentials': true
-        },
-        withCredentials: true
-    }).then(function (response) {
-        console.log(response);
-    })
+  axios.request({
+    url: 'http://localhost:3001/users',
+    method: 'get',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Access-Control-Allow-Credentials': true
+    },
+    withCredentials: true
+  }).then(function (response) {
+    console.log(response);
+  })
     .catch(function (error) {
-        console.log(error);
+      console.log(error);
     });
+
+  const routes = links.map((link, index) => {
+    return (
+      <Route path={link.path} key={index} >
+        {link.component}
+      </Route>
+    )
+  })
 
 
   return (
     <div>
       <h1>goppy</h1>
+      <Router>
+        <Switch>
+          {routes}
+        </Switch>
+      </Router>
     </div>
   );
 }
