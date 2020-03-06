eb5cea474700f6cc4ddbdc7117077391ee492815
Susan <susanwenzhang@gmail.com>
2020-03-06 17:29:47 -0500

    game data is set into state for single player

--- client/src/components/SingleGame/Index.js
+++ client/src/components/SingleGame/Index.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { withRouter, useLocation } from "react-router";
+import axios from 'axios';
 
 function SingleGame() {
   const gameId = useLocation().pathname.substring(20)
-  console.log("what's gameId", gameId)
+  const [state, setState] = useState({
+    game: {}
+  })
+
+  useEffect(() => {
+    axios.request({
+      url: `http://localhost:3001/games/${gameId}`,
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Access-Control-Allow-Credentials': true
+      },
+      withCredentials: true
+    }).then(function (response) {
+      setState(prev => ({
+        ...prev, 
+        game: response.data
+      }))
+    })
+      .catch(function (error) {
+        console.log(error);
+      });  
+  }, [])
+
 
   return (
     <div>
