339a8590cf767078db0dab5c5b33a3eb80de05fd
Susan <susanwenzhang@gmail.com>
2020-03-21 18:09:08 -0400

    started working on redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,21 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
+import { createStore } from 'redux';
 
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 axios.defaults.headers.common['Accept'] = 'application/json';
 axios.defaults.headers.common['Access-Control-Allow-Credentials'] = true;
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = 'http://localhost:3001';
+
+// STORE
+
+// ACTION
+
+// REDUCER - describe how your actions transform state
+
+// DISPATCH - this is what sends the action to the reducer
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
